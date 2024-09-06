4c67305d654b88af6abbd2de329c24a081ad3e69
f1st <dasmoo@mail.com>
2024-09-06 18:01:20 +0700

    Update model.py

--- model.py
+++ model.py
@@ -151,7 +151,7 @@ def train_model(token):
     predicted_price = model.predict(next_time_index)[0]  # Dự đoán giá
 
     # Xác định khoảng dao động xung quanh giá dự đoán
-    fluctuation_range = 0.01 * predicted_price  # Lấy 1% của giá dự đoán làm khoảng dao động
+    fluctuation_range = 0.015 * predicted_price  # Lấy 1% của giá dự đoán làm khoảng dao động
     min_price = predicted_price - fluctuation_range
     max_price = predicted_price + fluctuation_range
 
